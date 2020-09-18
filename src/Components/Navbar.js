f1d18760545a52a993a94841ee2dc33a9abe05f0
ColinShifley <ccshifley@gmail.com>
2020-09-18 17:50:52 -0400

    buttons register their id onClick

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <button>
+                <h1>Sign Now</h1>
+            </button>
+            <button>
+                <h1>Display Posts</h1>
+            </button>
+        </div>
+    );
+};
+
+export default Navbar;
