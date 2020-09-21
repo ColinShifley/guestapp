cb3e5cacf4932cbd033316be1081bcdff7f09f89
ColinShifley <ccshifley@gmail.com>
2020-09-20 22:12:07 -0400

    Added key/value pair to the page state  object

--- src/App.js
+++ src/App.js
@@ -6,40 +6,46 @@ import Wrapper from "./Components/Wrapper";
 
 class App extends React.Component {
     state = {
-        posts: {},
-        page: {}
-    }
+            posts: {},
+            page: {}
+        }
+
+
 
     //Custom Functions
-        addPost = post => {
-            //1. Take a copy of existing Posts
-            const posts = {...this.state.posts};
-            //2. add our new Posts to the Posts
-            posts[`post${Date.now()}`] = post;
-            //3. Set the new Posts objest to state
-            this.setState({posts});
-            console.log(post);
-        }
+    //     addPost = post => {
+    //         //1. Take a copy of existing Posts
+    //         const posts = {...this.state.posts};
+    //         //2. add our new Posts to the Posts
+    //         posts[`post${Date.now()}`] = post;
+    //         //3. Set the new Posts objest to state
+    //         this.setState({posts});
+    //         console.log(post);
+    //     }
 
         activePage = whichPage => {
+                //trying to fix Asynchronous issue with Page
+                this.setState(() => ({
+                    page: whichPage
+                }), () => {
+                    console.log(this.state.page)
+                });
 
-                this.setState({page: whichPage});
-                console.log(this.state.page)
+console.log(this.state.page)
+                // Code below works kind of
+                // this.setState({page: whichPage})
+                // console.log(this.state.page)
         }
 
-    // componentDidMount() {
-    //     activePage;
-    // }
 
     render() {
         return (
             <div className="App">
-                <div className="App">
                     <header className="App-header">
                         <img className={'LogoHeader'} src={logo} alt={'logo'}/>
                     </header>
-                    <Wrapper activePage={this.activePage}/>
-                </div>
+                    <Wrapper activePage={this.activePage}></Wrapper>
+                <h1>{Object.keys(this.state.page).map(key => <p key={key}>{key}</p> )}</h1>
             </div>
         );
     }
