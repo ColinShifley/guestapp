ed43b67467f2c5eb2e8badc7f8b83d5c8a756184
ColinShifley <ccshifley@gmail.com>
2020-10-13 07:45:35 -0400

    form displaying

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import logo from "./Components/images/logo.png";
 import Wrapper from "./Components/Wrapper";
+import SignNow from "./Components/SignNow";
+import DisplayPosts from "./Components/DisplayPosts";
 
 
 class App extends React.Component {
@@ -12,40 +14,53 @@ class App extends React.Component {
 
 
 
-    //Custom Functions
-    //     addPost = post => {
-    //         //1. Take a copy of existing Posts
-    //         const posts = {...this.state.posts};
-    //         //2. add our new Posts to the Posts
-    //         posts[`post${Date.now()}`] = post;
-    //         //3. Set the new Posts objest to state
-    //         this.setState({posts});
-    //         console.log(post);
-    //     }
+    // Custom Functions
+        addPost = post => {
+            //1. Take a copy of existing Posts
+            const posts = {...this.state.posts};
+            //2. add our new Posts to the Posts
+            posts[`post${Date.now()}`] = post;
+            //3. Set the new Posts objest to state
+            this.setState({posts});
+            console.log(post);
+        }
 
         activePage = whichPage => {
+                let currentPage = {...this.state.page};
                 //trying to fix Asynchronous issue with Page
+                currentPage = whichPage;
                 this.setState(() => ({
-                    page: whichPage
+                    page: currentPage
                 }), () => {
                     console.log(this.state.page)
                 });
 
-console.log(this.state.page)
+
                 // Code below works kind of
                 // this.setState({page: whichPage})
                 // console.log(this.state.page)
         }
+    pageFunc = () => {
+        if (this.state.page === SignNow) {
+            console.log('SignNowPage')
+            return <SignNow addPost={this.addPost}/>
+        } if (this.state.page === DisplayPosts) {
+            console.log('DisplayPostsSSSSSS')
+        }
+    }
+
 
 
     render() {
+
         return (
             <div className="App">
                     <header className="App-header">
                         <img className={'LogoHeader'} src={logo} alt={'logo'}/>
                     </header>
                     <Wrapper activePage={this.activePage}></Wrapper>
-                <h1>{Object.keys(this.state.page).map(key => <p key={key}>{key}</p> )}</h1>
+                <h1>{Object.keys(this.state.page).map(key => <SignNow key={key} details={this.state.page[key]}/>)}</h1>
+
             </div>
         );
     }
--- src/Components/SignNow.js
+++ src/Components/SignNow.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
+import './SignNow.css'
+
 
 
 class SignNow extends Component {
+    nameRef = React.createRef();
+    titleRef = React.createRef();
+    commentRef = React.createRef();
+
+    createPost = (event) => {
+        //1. Stop form from Submitting
+        event.preventDefault();
+        const post = {
+            name: this.nameRef.current.value,
+            title: this.titleRef.current.value,
+            comment: this.commentRef.current.value
+        };
+        this.props.addPost(post);
+        event.currentTarget.reset();
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <form className={'post-edit'} onSubmit={this.createPost}>
+                <input name={'name'} ref={this.nameRef} type={'text'} placeholder={'Name'}/>
+                <input name={'title'} ref={this.titleRef} type={'text'} placeholder={'Title'}/>
+                <input name={'comment'} ref={this.commentRef} type={'text'} placeholder={'Comment'}/>
+                <button type={'submit'}>Create New Post</button>
+            </form>
         );
     };
 };
