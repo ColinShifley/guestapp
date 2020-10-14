277dc8929f1c8e2a97503c4fa516723dee72ac1f
ColinShifley <ccshifley@gmail.com>
2020-10-14 19:50:56 -0400

    Form works from SignNow but only if in App file currently

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 import logo from "./Components/images/logo.png";
 import Wrapper from "./Components/Wrapper";
-import SignNow from "./Components/SignNow";
-import DisplayPosts from "./Components/DisplayPosts";
+//import SignNow from "./Components/SignNow";
+//import DisplayPosts from "./Components/DisplayPosts";
 
 
 class App extends React.Component {
-    state = {
-            posts: {},
-            page: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: '',
+            page: ''
         }
-
-
+    }
 
     // Custom Functions
         addPost = post => {
@@ -34,22 +35,7 @@ class App extends React.Component {
                 }), () => {
                     console.log(this.state.page)
                 });
-
-
-                // Code below works kind of
-                // this.setState({page: whichPage})
-                // console.log(this.state.page)
         }
-    pageFunc = () => {
-        if (this.state.page === SignNow) {
-            console.log('SignNowPage')
-            return <SignNow addPost={this.addPost}/>
-        } if (this.state.page === DisplayPosts) {
-            console.log('DisplayPostsSSSSSS')
-        }
-    }
-
-
 
     render() {
 
@@ -58,9 +44,12 @@ class App extends React.Component {
                     <header className="App-header">
                         <img className={'LogoHeader'} src={logo} alt={'logo'}/>
                     </header>
-                    <Wrapper activePage={this.activePage}></Wrapper>
-                <h1>{Object.keys(this.state.page).map(key => <SignNow key={key} details={this.state.page[key]}/>)}</h1>
 
+                    <Wrapper activePage={this.activePage}></Wrapper>
+
+                {/* (This was to test if it worked)
+                <h1>{Object.keys(this.state.page).map(key => <SignNow key={key} details={this.state.page[key]}/>)}</h1>
+                */}
             </div>
         );
     }
--- src/Components/CommentContainerBox.js
+++ src/Components/CommentContainerBox.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './CommentContainerBox.css';
 import Navbar from "./Navbar";
+import SignNow from "./SignNow";
 import Comment from "./Comment";
 
 class CommentContainerBox extends Component {
@@ -10,6 +11,10 @@ class CommentContainerBox extends Component {
             <Navbar {...this.props}/>
             <div className={'innerBlueBoxContainer'}>
                 <div className={'blueBox'}>
+
+                    {/*Having trouble gaining Access to function in App*/}
+                    <SignNow addPost={this.props.addPost}></SignNow>
+
                     <Comment image={'image'} title={'title'} comment={'comment'}/>
                 </div>
             </div>
